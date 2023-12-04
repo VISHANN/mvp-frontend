49a2660aaba7785c70718add609c9b80213d0874
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2023-12-04 16:39:42 +0530

    useEffect to get a logged in user(if exists) from server  on mounting of Providers

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,20 @@ import { useContext } from 'react'
 
 export default function Home() {
   const [user, dispatch] = useContext(UserContext);
+  function handleClick(e){
+    // TEST FUNCTION: Delete after use;
 
+    fetch('http://localhost:8000/api/v1/me', {
+      credentials: "include",
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
   return (
     <main>
       <Navbar />
-      {user.picture}
       <br />
-      {JSON.parse(localStorage.getItem('user')).picture}
+      {/* <button onClick={handleClick}> Get User </button> */}
     </main>
   )
 }
--- src/components/providers.js
+++ src/components/providers.js
@@ -1,7 +1,8 @@
 "use client"
 
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import { createContext, useReducer, useState } from "react"
+import { createContext, useEffect, useReducer } from "react"
+import { useRouter } from "next/navigation";
 
 export const UserContext = createContext(null);
 
@@ -20,6 +21,31 @@ export default function Providers ({ children }){
     given_name: ""
   }
   const [user, dispatch] = useReducer(reducer, initialUser);
+  const router = useRouter();
+
+  useEffect(() => {
+    // This effect exists only to load a logged in user's data is session exists on startup.
+    fetch('http://localhost:8000/api/v1/me', {
+      credentials: "include",
+    })
+    .then(res => {
+      if(res.status !== 200) {
+        // user's server session is missing; redirect to login
+        
+        router.push('/signin');
+        throw new Error('Server Session Missing');
+      }
+      return res.json();
+    })
+    .then(user => {
+      const { given_name, picture } = user;
+      dispatch({ type: 'ADD_USER', payload: { given_name, picture }});
+      return user;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, []);
 
   return (
     <UserContext.Provider value={[user, dispatch]}>
