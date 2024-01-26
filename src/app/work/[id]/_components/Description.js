f703d65578c6f24f55622002ee5913d61675ec38
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-01-26 18:37:16 +0530

    move work description to its own component

---
+++ src/app/work/[id]/_components/Description.js
@@ -0,0 +1,48 @@
+"use client"
+
+import { useState } from "react";
+
+const styles = {
+  textAlign: 'justify',
+}
+
+export default function Description ({ children }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  if ( !children ) {
+    return(
+      <p>
+        This book does not have a description yet.
+      </p>
+    )
+  }
+  
+  return (
+    <div>
+      <p style={styles}>
+        {(isVisible) ? children : truncate(children)}
+      </p>
+      <button
+        className='link-primary capitalize'
+        onClick={handleClick} > 
+        {isVisible ? 'Show Less' : 'Show More'}
+      </button>
+    </div>
+  )
+  
+  function handleClick (e) {
+    setIsVisible(state => !state);
+  }
+}
+
+function truncate (text) {
+  // vary the length of truncated text based on screen sizes.
+  const MAX = (document.documentElement.clientWidth < 760) ? 160 : 308;
+
+  if ( text.length < MAX) {
+    return text;
+  } else {
+    let subString = text.slice(0, MAX);
+    return (<>{subString.slice(0, subString.lastIndexOf(' '))} &hellip;</>);
+  }
+}
--- src/app/work/[id]/page.js
+++ src/app/work/[id]/page.js
@@ -2,19 +2,9 @@ import styles from './page.module.css'
 import Image from 'next/image'
 import ShelvesButton from '@/components/ShelvesBtn'
 import PrimaryLink from '@/components/PrimaryLink'
+import Description from './_components/Description'
 
 const description = "Draws on more than forty interviews with Steve Jobs, as well as interviews with family members, friends, competitors, and colleagues, to offer a look at the co-founder and leading creative force behind the Apple computer company.\nThis biography shares the life and personality of a creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing."
-function truncate (text) {
-  // vary the length of truncated text based on screen sizes.
-  const MAX = (document.documentElement.clientWidth < 760) ? 160 : 308;
-
-  if ( text.length < MAX) {
-    return text;
-  } else {
-    let subString = text.slice(0, MAX);
-    return (<>{subString.slice(0, subString.lastIndexOf(' '))} &hellip;</>);
-  }
-}
 
 async function getWorkMetadata() {
   const data = await fetch('https://openlibrary.org/works/OL45804W.json').then(res => res.json());
@@ -22,31 +12,14 @@ async function getWorkMetadata() {
 }
 
 export default async function Work() {
-  // const [isVisible, setIsVisible] = useState(false);
-  // const [work, setWork] = useState(null);
-
-  // function handleClick (e) {
-  //   setIsVisible(state => !state);
-  // }
-
-  // useEffect(() => {
-  //   fetch('https://openlibrary.org/works/OL45804W.json')
-  //   .then(res => res.json())
-  //   .then(data => { 
-  //     setWork(data);
-  //     console.log(data);
-  //   })
-  //   .catch(err => console.error(err))
-
-  // }, [])
-
-  // if (!work) {
-  //   return (
-  //     <>Loadin...</>
-  //   )
-  // }
-
   const work = await getWorkMetadata();
+
+  console.log(work); // delete
+  if (!work) {
+    return (
+      <>Loadin...</>
+    )
+  }
 
   return (
     <main className={`container ${styles.main}`}>
@@ -81,16 +54,9 @@ export default async function Work() {
                 </PrimaryLink>
               </h2>
             </div>
-            <div className={styles.description}>
-              <p>
-                {description}
-              </p>
-              {/* <button
-                className='link-primary capitalize'
-                onClick={handleClick} > 
-                {isVisible ? 'Show Less' : 'Show More'}
-              </button> */}
-            </div>
+            <Description>
+              {work.description}
+            </Description>
           </div>
         </section>
       </div>
