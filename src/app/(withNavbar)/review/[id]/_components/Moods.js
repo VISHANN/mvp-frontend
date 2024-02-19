53f80f33999370eb51ead10fd20fc7dd4df66276
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-02-19 23:53:34 +0530

    create DoubleThumbsUp make rating button feedback meaningful with relevant colors

--- src/app/(withNavbar)/review/[id]/_components/Moods.js
+++ src/app/(withNavbar)/review/[id]/_components/Moods.js
@@ -1,23 +1,22 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 export default function Moods({ value, moodsList, handleChange }) {
   return (
     <ul className={styles.moods}>
-      {moodsList.map(mood => (
+      {moodsList.map((mood) => (
         <li key={mood.id}>
-          <input 
-            name='moods'
-            type='checkbox'
+          <input
+            name="moods"
+            type="checkbox"
             value={mood.id}
             id={`mood_id_${mood.id}`}
             checked={value[mood.id]}
-            onChange={handleChange} />
+            onChange={handleChange}
+          />
 
-          <label htmlFor={`mood_id_${mood.id}`}>
-            {mood.name}
-          </label>
+          <label htmlFor={`mood_id_${mood.id}`}>{mood.name}</label>
         </li>
       ))}
     </ul>
-  )
+  );
 }
--- src/app/(withNavbar)/review/[id]/_components/Ratings.js
+++ src/app/(withNavbar)/review/[id]/_components/Ratings.js
@@ -1,55 +1,65 @@
-import { BsHandThumbsUp, BsHandThumbsDown, BsHandThumbsUpFill, BsHandThumbsDownFill } from "react-icons/bs";
-import styles from './index.module.css'
+import {
+  BsHandThumbsUp,
+  BsHandThumbsDown,
+  BsHandThumbsUpFill,
+  BsHandThumbsDownFill,
+} from "react-icons/bs";
+import styles from "./index.module.css";
 
 export default function Ratings({ value, ratingProps, handleChange }) {
-  return(
+  return (
     <div className={styles.ratingButtons}>
-      {ratingProps.map(rating => {
+      {ratingProps.map((rating) => {
         return (
           <div key={rating.id}>
-            <input 
+            <input
               type="radio"
-              name='rating'
+              name="rating"
               id={`rating_${rating.id}`}
               className={styles.rating}
               value={rating.id}
               checked={value === rating.id}
-              onChange={handleChange} />
-      
+              onChange={handleChange}
+            />
+
             <label htmlFor={`rating_${rating.id}`}>
-              <RatingBtn 
-                value={value}
-                rating={rating} />
+              <RatingBtn value={value} rating={rating} />
             </label>
-        </div>
-        )
+          </div>
+        );
       })}
     </div>
-  )
+  );
 }
 
 function RatingBtn({ value, rating }) {
-  let caption = rating.caption, icon;
+  let caption = rating.caption,
+    icon;
 
   switch (rating.id) {
-    case '0':
-      icon = (value === '0' ? <BsHandThumbsDownFill /> : <BsHandThumbsDown />)
+    case "0":
+      icon = value === "0" ? <BsHandThumbsDownFill /> : <BsHandThumbsDown />;
       break;
-    case '1': 
-      icon = (value === '1' ? <BsHandThumbsUpFill /> : <BsHandThumbsUp />)
+    case "1":
+      icon = value === "1" ? <BsHandThumbsUpFill /> : <BsHandThumbsUp />;
       break;
-    case '2':
-      icon = (value === '2' ? <BsHandThumbsUpFill /> : <BsHandThumbsUp />)
+    case "2":
+      icon = <DoubleThumbsUp />;
       break;
   }
   return (
     <div className={styles.ratingBtn}>
-      <div className={styles.thumbs}>
-        {icon}
-      </div>
-      <p className={styles.caption}>
-        {caption}
-      </p>
+      <div className={styles.thumbs}>{icon}</div>
+      <p className={styles.caption}>{caption}</p>
     </div>
-  )
+  );
+}
+
+function DoubleThumbsUp() {
+  return (
+    <div className={styles.doubleThumbsUp}>
+      <BsHandThumbsUpFill />
+      <BsHandThumbsUpFill />
+    </div>
+  );
 }
