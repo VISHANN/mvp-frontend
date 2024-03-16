99c2b1880310689af0d9fa93b54ae9ba19c34105
Ujjwal Goswami <19ucc081@lnmiit.ac.in>
2024-03-16 17:44:13 +0530

    add padding to Review component

--- src/app/(withNavbar)/work/[id]/_components/Review.js
+++ src/app/(withNavbar)/work/[id]/_components/Review.js
@@ -3,30 +3,30 @@ import UserProfile from "@/components/UserProfile";
 import RatingIcon from "@/components/RatingIcon";
 
 export default function Review({ review, children }) {
-  const user = review.author;
-
   return (
-    <div className={styles.gridWrap}>
-      <div className={styles.left}>
-        <UserProfile src={user.picture} />
-      </div>
-      <div className={styles.right}>
-        <div className={styles.header}>
-          <span>{user.given_name}</span>
-          <span> • </span>
-          <span className={styles.ratingIcon}>
-            <RatingIcon ratingId={review.rating} active={true} />
-          </span>
+    <div className={styles.review}>
+      <div className={styles.gridWrap}>
+        <div className={styles.left}>
+          <UserProfile src={review.author.picture} />
         </div>
-        <div>
-          <p>{review.text}</p>
-        </div>
-        <div>
-          <span className={styles.time}>
-            {Date(review.createdAt).substring(4, 15)}
-          </span>
-          <span> • </span>
-          <span className={styles.time}>{children}</span>
+        <div className={styles.right}>
+          <div className={styles.header}>
+            <span>{review.author.username}</span>
+            <span> • </span>
+            <span className={styles.ratingIcon}>
+              <RatingIcon ratingId={review.rating} active={true} />
+            </span>
+          </div>
+          <div>
+            <p>{review.text}</p>
+          </div>
+          <div>
+            <span className={styles.time}>
+              {Date(review.createdAt).substring(4, 15)}
+            </span>
+            <span> • </span>
+            <span className={styles.time}>{children}</span>
+          </div>
         </div>
       </div>
     </div>
